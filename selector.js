ca4742575633a92e5da952487b78a815bdb64719
Brittney Hanna <brittneyhanna@yahoo.com>
2026-03-17 16:19:17 -0500

    Stabilize structure, enforce grid logic, ensure image folders, remove unused directory

--- selector.js
+++ selector.js
@@ -1,23 +1,65 @@
-const tiles = document.querySelectorAll(".tile");
+fetch('data/concepts.json')
+.then(res => res.json())
+.then(concepts => {
 
-tiles.forEach((tile, index) => {
+  const container = document.querySelector(".container");
 
-const num = index + 1;
+  const positions = [
+    { top: 300, left: 80 },
+    { top: 300, left: 360 },
+    { top: 300, left: 640 },
+    { top: 300, left: 920 },
 
-const xOverlay = document.createElement("div");
+    { top: 620, left: 80 },
+    { top: 620, left: 360 },
+    { top: 620, left: 640 },
+    { top: 620, left: 920 }
+  ];
 
-xOverlay.style.position = "absolute";
+  concepts.forEach((c, index) => {
 
-/* POSITION THIS EXACTLY OVER THE "X" IN YOUR TEMPLATE */
-xOverlay.style.top = "170px";
-xOverlay.style.left = "140px";
+    if (!positions[index]) return;
 
-xOverlay.style.color = "gold";
-xOverlay.style.fontSize = "22px";
-xOverlay.style.fontWeight = "bold";
+    const conceptNum = String(c.id).padStart(3, "0");
 
-xOverlay.innerText = num;
+    const tile = document.createElement("div");
+    tile.style.position = "absolute";
+    tile.style.top = positions[index].top + "px";
+    tile.style.left = positions[index].left + "px";
+    tile.style.width = "240px";
+    tile.style.height = "240px";
 
-tile.appendChild(xOverlay);
+    const preview = document.createElement("div");
+    preview.style.width = "100%";
+    preview.style.height = "100%";
+    preview.style.display = "flex";
+    preview.style.alignItems = "center";
+    preview.style.justifyContent = "center";
+    preview.style.overflow = "hidden";
+
+    const img = document.createElement("img");
+
+    img.src = `Concepts/Concept_${conceptNum}/images/Concept_${conceptNum}_01.png`;
+
+    img.style.width = "100%";
+    img.style.height = "100%";
+    img.style.objectFit = "cover";
+
+    img.onload = () => {
+      preview.appendChild(img);
+    };
+
+    img.onerror = () => {
+      preview.innerHTML = "<div style='color:gold;'>Coming Soon</div>";
+    };
+
+    tile.onclick = () => {
+      window.location.href = `preview.html?concept=${c.id}`;
+    };
+
+    tile.appendChild(preview);
+    container.appendChild(tile);
+
+  });
 
 });
